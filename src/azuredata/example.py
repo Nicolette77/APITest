9a07ab92b085b460e9a0c64f51c789b890e671be
NicolettevandenHeeve <nicoletteatwork77@gmail.com>
2022-10-20 11:12:06 +0200

    Data written to output.json.

--- src/azuredata/example.py
+++ src/azuredata/example.py
@@ -1,9 +1,6 @@
 import base64
 import json
 import pprint as pp
-
-import fp as fp
-import pandas as pd
 import requests
 from requests import Response
 
@@ -23,55 +20,9 @@ HEADERS = {
     'Authorization': 'Basic %s' % B64USERPASS
 }
 
-try:
-    ADO_RESPONSE: Response = requests.get(
-        ORGANIZATION_URL + RESOURCE_PATH, headers=HEADERS)
+Response = requests.get(
+        ORGANIZATION_URL + RESOURCE_PATH, headers=HEADERS).json()
 
-    pp.pprint(ADO_RESPONSE)
-    pp.pprint(ADO_RESPONSE.text)
-    ADO_RESPONSE.raise_for_status()
-except requests.exceptions.HTTPError as err:
-    pp.pprint(err)
-
-# Read response and write to output.json (Does not write file yet)
-with open('output.json', 'w') as ResponseOutput:
-    ResponseOutput.write('output.json')
-    print(ResponseOutput)
-
-# BuildInformation_list = []
-#
-# with open('output.json') as BuildInfoCollect:
-#     BuildInfo = json.load(BuildInfoCollect)
-#     for item in BuildInfo:
-#         buildId = item['id']
-#         buildNumber = item['buildNumber']
-#         buildName = item['definition']['name']
-#         startTime = item['startTime']
-#         finishTime = item['finishTime']
-#
-#         BuildInfo = {
-#             'id': buildId,
-#             'buildNumber': buildNumber,
-#             'name': buildName,
-#             'startTime': startTime,
-#             'finishTime': finishTime
-#         }
-#     print(data[0])
-#     print(ResponseOutput['value'][0]['buildNumber'])
-#     print(ResponseOutput['value'][0]['definition']['name'])
-#     print(ResponseOutput['value'][0]['startTime'])
-#     print(ResponseOutput['value'][0]['finishTime'])
-#     file.write(json.dumps(output.json(), indent=4))
-#     file.close()
-#     BuildInformation_list.append(BuildInfo)
-#     BuildInfo = open('BuildInfoData.csv', 'w')
-#     csv_writer = csv.writer(open('BuildInfoData.csv', 'w'))
-#
-#     count = 0
-#     for item in ResponseOutput:
-#         if count == 0:
-#             header = ResponseOutput.keys()
-#             csv_writer.writerow(header)
-#             count += 1
-#     csv_writer.writerow(ResponseOutput.values())
-# BuildInfo.close()
+with open('ouput.json', 'w') as output_file:
+    json.dump(Response, output_file)
+print(output_file)
